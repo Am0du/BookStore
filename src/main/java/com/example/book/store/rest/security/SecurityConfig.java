d14d1f118fd9fea02ea6b23d04a2621de6a58844
Amodu <amodudavid777@gmail.com>
2024-04-23 13:38:33 +0100

    Feat: Added Bcrpyt to encode password

---
+++ src/main/java/com/example/book/store/rest/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.book.store.rest.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
