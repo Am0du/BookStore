d3f3d8272e2779765ee341809cd1e9187de315cb
Amodu <amodudavid777@gmail.com>
2024-04-23 16:46:19 +0100

    Feat: Added Basic authenication to User

--- src/main/java/com/example/book/store/rest/service/UserService.java
+++ src/main/java/com/example/book/store/rest/service/UserService.java
@@ -3,15 +3,18 @@ package com.example.book.store.rest.service;
 import com.example.book.store.rest.entity.User;
 import com.example.book.store.rest.response.MultipleResponse;
 import com.example.book.store.rest.response.SingleResponse;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Map;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 
     MultipleResponse<User> findAllUsers();
     SingleResponse<User> findUser(String email);
 
     SingleResponse<User> addUser(User user);
 
-    boolean deleteUser(User user);
+    SingleResponse<User> editUser(User user);
+
+    SingleResponse<User> deleteUser(String email);
 }
--- src/main/java/com/example/book/store/rest/service/UserServiceImpl.java
+++ src/main/java/com/example/book/store/rest/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.book.store.rest.service;
 
+import com.example.book.store.rest.entity.Authority;
+import com.example.book.store.rest.entity.Book;
 import com.example.book.store.rest.entity.User;
 import com.example.book.store.rest.exception.UserDataNotComplete;
 import com.example.book.store.rest.exception.UserNotFound;
@@ -9,9 +11,18 @@ import com.example.book.store.rest.response.SingleResponse;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,33 +33,44 @@ public class UserServiceImpl implements UserService {
 
     private SingleResponse<User> singleResponse;
 
+    private PasswordEncoder passwordEncoder;
+
     public UserServiceImpl(){}
 
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository,
                            MultipleResponse<User> multipleResponse,
-                           SingleResponse<User> singleResponse){
+                           SingleResponse<User> singleResponse,
+                           PasswordEncoder passwordEncoder){
 
         this.userRepository = userRepository;
         this.multipleResponse = multipleResponse;
         this.singleResponse = singleResponse;
+        this.passwordEncoder = passwordEncoder;
 
+    }
+
+    private MultipleResponse<User> multipleResponse(List<User> user, boolean success){
+        multipleResponse.setEntityList(user);
+        multipleResponse.setSuccessfull(success);
+        return multipleResponse;
+    }
+
+    private SingleResponse<User> singleResponse(User user, boolean success){
+        singleResponse.setIssucessfull(success);
+        singleResponse.setEntity(user);
+        return singleResponse;
     }
     @Override
     public MultipleResponse<User> findAllUsers() {
-
-        multipleResponse.setEntityList(userRepository.findAll());
-        multipleResponse.setSuccessfull(true);
-        return multipleResponse;
+        return multipleResponse(userRepository.findAll(), true);
     }
 
     @Override
     public SingleResponse<User> findUser(String email) {
         try {
-            singleResponse.setEntity(userRepository.findByEmail(email));
-            singleResponse.setIssucessfull(true);
-            return singleResponse;
+            return singleResponse(userRepository.findByEmail(email), true);
         }catch (EntityNotFoundException exc){
             throw new UserNotFound("User with email address");
         }
@@ -59,20 +81,49 @@ public class UserServiceImpl implements UserService {
     public SingleResponse<User> addUser(User user) {
         try {
             User newUser = userRepository.save(user);
-            singleResponse.setEntity(newUser);
-            singleResponse.setIssucessfull(true);
-            return singleResponse;
+            newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+            return singleResponse(newUser, true);
         }catch (Exception  exc){
             throw new UserDataNotComplete(exc.getMessage());
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                mapRolesToAuthorities(user.getAuthority()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Authority> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getUserEmail())).collect(Collectors.toList());
+    }
 
     @Override
     @Transactional
-    public boolean deleteUser(User user) {
+    public SingleResponse<User> editUser(User user) {
+        try {
+            User existingUser = userRepository.findByEmail(user.getEmail());
+            existingUser.setFirstName(user.getFirstName() != null ? user.getFirstName() : existingUser.getFirstName());
+            existingUser.setLastName(user.getLastName() != null ? user.getLastName() : existingUser.getLastName());
+            existingUser.setMiddleName(user.getMiddleName() != null ? user.getMiddleName() : existingUser.getMiddleName());
+            existingUser.setPassword(user.getPassword() != null ? user.getPassword() : existingUser.getPassword());
+            return  singleResponse(userRepository.save(existingUser), true);
+        }catch (Exception exc){
+            throw new UserNotFound("No user with " + user.getEmail() + "found");
+        }
+
+    }
+
+    @Override
+    @Transactional
+    public SingleResponse<User> deleteUser(String email) {
+        User user = userRepository.findByEmail(email);
         userRepository.delete(user);
-        return true;
+        return singleResponse(user, true);
 
     }
 }
