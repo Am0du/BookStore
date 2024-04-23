188fb241c1b6f7c7b0e08c903ff9f50beae336f3
Amodu <amodudavid777@gmail.com>
2024-04-23 16:51:33 +0100

    Refactor: genre to return a list of books

--- src/main/java/com/example/book/store/rest/repository/BookRepository.java
+++ src/main/java/com/example/book/store/rest/repository/BookRepository.java
@@ -4,8 +4,10 @@ import com.example.book.store.rest.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    Book findByGenre(String genre);
+    List<Book> findByGenre(String genre);
     Book findByTitle(String title);
 }
