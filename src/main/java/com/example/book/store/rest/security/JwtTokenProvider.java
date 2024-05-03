d5e8ee94585cc821872fdd0af16f51c2b2c7059b
Amodu <amodudavid777@gmail.com>
2024-05-03 19:59:51 +0100

    Feat: Added JWT token

---
+++ src/main/java/com/example/book/store/rest/security/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package com.example.book.store.rest.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+import java.security.Key;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app-jwt-expiration}")
+    private long jwtExpirationDate;
+
+    private Key key(){
+
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+
+    public String generateToken(Authentication authentication){
+        String email = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+
+    }
+
+    public String getEmail(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey((SecretKey) key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+
+    }
+
+    public boolean validateToken(String token){
+        Jwts.parserBuilder()
+                .setSigningKey((SecretKey) key())
+                .build().parse(token);
+        return true;
+    }
+
+
+
+
+}
