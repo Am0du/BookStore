f1e2c13a8843db26a885cd11fdd392c213a0254c
Amodu <amodudavid777@gmail.com>
2024-05-06 16:03:42 +0100

    Refactor: Code clean up aswell as lazy initialization of JwtAuthFilter to fix circular dependency bug

--- src/main/java/com/example/book/store/rest/security/SecurityConfig.java
+++ src/main/java/com/example/book/store/rest/security/SecurityConfig.java
@@ -1,29 +1,30 @@
 package com.example.book.store.rest.security;
 
-import com.example.book.store.rest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.net.PasswordAuthentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
+    private JwtAuthFilter authenticationFilter;
+    @Autowired
+    public SecurityConfig(@Lazy JwtAuthFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -43,15 +44,26 @@ public class SecurityConfig{
         http.authorizeHttpRequests(configure ->
                 configure
 
-                        .requestMatchers(HttpMethod.POST, "api/v1/users/signup").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/users/signin").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "api/v1/users").hasAnyRole("USER", "MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "api/v1/users").hasAnyRole("USER", "MANAGER")
-                        .requestMatchers(HttpMethod.GET, "api/v1/users").hasAnyRole("USER", "MANAGER")
-                        .requestMatchers(HttpMethod.GET, "api/v1/users/**").hasAnyRole("USER", "MANAGER")
+//                        .requestMatchers(HttpMethod.POST, "api/v1/users/signup").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/users/signin").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
+//                        .requestMatchers(HttpMethod.PUT, "api/v1/users").hasAnyRole("USER", "MANAGER")
+//                        .requestMatchers(HttpMethod.DELETE, "api/v1/users").hasAnyRole("USER", "MANAGER")
+//                        .requestMatchers(HttpMethod.GET, "api/v1/users").hasAnyRole("USER", "MANAGER")
+//                        .requestMatchers(HttpMethod.GET, "api/v1/users/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                        .requestMatchers("/api/v1/users/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
 
-        );
+    );
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.httpBasic(Customizer.withDefaults());
+
+
+
 
 
         http.csrf(AbstractHttpConfigurer::disable);
