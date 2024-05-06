7214ea338aae01906bd42f0948264b86e8caba0f
Amodu <amodudavid777@gmail.com>
2024-05-06 15:58:06 +0100

    Refactor: Bugfix to get the principal username instead of name

--- src/main/java/com/example/book/store/rest/security/JwtTokenProvider.java
+++ src/main/java/com/example/book/store/rest/security/JwtTokenProvider.java
@@ -1,10 +1,12 @@
 package com.example.book.store.rest.security;
 
+import com.example.book.store.rest.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -29,12 +31,12 @@ public class JwtTokenProvider {
 
 
     public String generateToken(Authentication authentication){
-        String email = authentication.getName();
+        UserDetails user = (UserDetails) authentication.getPrincipal();
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
 
         String token = Jwts.builder()
-                .setSubject(email)
+                .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
                 .signWith(key())
