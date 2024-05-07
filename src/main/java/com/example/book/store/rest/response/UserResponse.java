9da3b56adf13e873c2c749a4bccf10fdd7d62bb6
Amodu <amodudavid777@gmail.com>
2024-05-07 14:15:59 +0100

    Feat: User response class

---
+++ src/main/java/com/example/book/store/rest/response/UserResponse.java
@@ -0,0 +1,107 @@
+package com.example.book.store.rest.response;
+
+import com.example.book.store.rest.entity.Authority;
+import com.example.book.store.rest.entity.Book;
+import com.example.book.store.rest.entity.Comment;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserResponse {
+    private long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private int active;
+    private List<Book> books;
+    private List<Comment> comments;
+    private List<Authority> authority;
+
+    public UserResponse(long id, String firstName, String middleName, String lastName, String email, int active, List<Book> books, List<Comment> comments, List<Authority> authority) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.active = active;
+        this.books = books;
+        this.comments = comments;
+        this.authority = authority;
+    }
+
+    public UserResponse(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Authority> getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(List<Authority> authority) {
+        this.authority = authority;
+    }
+}
