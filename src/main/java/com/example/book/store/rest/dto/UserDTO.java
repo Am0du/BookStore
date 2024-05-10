358973aa899ff0cd1d676564efcaab2911d84f3b
Amodu <amodudavid777@gmail.com>
2024-05-10 20:26:07 +0100

    Feat: User Data transfer object

---
+++ src/main/java/com/example/book/store/rest/dto/UserDTO.java
@@ -0,0 +1,69 @@
+package com.example.book.store.rest.dto;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String password;
+
+    public UserDTO(String firstName, String lastName, String middleName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
