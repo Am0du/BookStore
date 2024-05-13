ed070c247c224c328b15abe6abeb1a7d6c7e6c75
Amodu <amodudavid777@gmail.com>
2024-05-13 21:54:12 +0100

    Refactor: added title to DTO

--- src/main/java/com/example/book/store/rest/dto/CommentDTO.java
+++ src/main/java/com/example/book/store/rest/dto/CommentDTO.java
@@ -8,10 +8,13 @@ public class CommentDTO {
 
     private String comment;
 
-    public CommentDTO(long bookId, long commentId, String comment) {
+    private String title;
+
+    public CommentDTO(long bookId, long commentId, String comment, String title) {
         this.bookId = bookId;
         this.commentId = commentId;
         this.comment = comment;
+        this.title = title;
     }
 
     public long getBookId() {
@@ -36,6 +39,14 @@ public class CommentDTO {
 
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     @Override
