943849c42800f2bdbb33154eb2d4ddcb0fedc5cc
Amodu <amodudavid777@gmail.com>
2024-05-13 18:26:55 +0100

    Refactor: changed timestamp to UTC

--- src/main/java/com/example/book/store/rest/exceptionHandler/Errorhandler.java
+++ src/main/java/com/example/book/store/rest/exceptionHandler/Errorhandler.java
@@ -3,10 +3,14 @@ package com.example.book.store.rest.exceptionHandler;
 import com.example.book.store.rest.exception.*;
 import com.example.book.store.rest.response.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @ControllerAdvice
 public class Errorhandler {
@@ -22,27 +26,30 @@ public class Errorhandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(UserDoesNotHaveAuthority exc){
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(UserNotFound exc){
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(UserDataNotComplete exc){
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
@@ -50,55 +57,74 @@ public class Errorhandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(Exception exc){
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(InvalidRole exc) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(BookEditNotPermitted exc) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
-        errorResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(BookDoesNotExist exc) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(CommentNotFound exc) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
         errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
+
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
+        errorResponse.setMessage("Data integrity violation: " + ex.getRootCause().getMessage());
+        errorResponse.setStatus(HttpStatus.CONFLICT.value());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+
+    }
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(BookAlreadyExist exc) {
+        OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+        errorResponse.setTimestamp(timestamp);
         errorResponse.setMessage(exc.getMessage());
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
